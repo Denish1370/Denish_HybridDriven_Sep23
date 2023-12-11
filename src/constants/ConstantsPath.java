a382d1dd62acdd3a9ce17da990d41b7041c1c3e5
Denish Santoki <denishsantoki74@.com>
2023-12-11 11:12:40 +0530

    Framework_Day2_Changes_Denish

--- src/constants/ConstantsPath.java
+++ src/constants/ConstantsPath.java
@@ -5,4 +5,6 @@ public class ConstantsPath {
 	public final static String CHROME_DRIVER_KEY = "webdriver.chrome.driver";
 	public final static String CHROME_DRIVER_VALUE = ".//chromeDriver/chromedriver.exe";
 	public final static String DEV_ENV_FILEPATH = ".//config/dev-env.properties";
+	public final static int WAIT = 30;
+	public final static int FAST_WAIT = 5;
 }
